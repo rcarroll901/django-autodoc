7cf516838099b01fd5d8b0d3a1b9ee54ca597195
Ryan Carroll <raec901@gmail.com>
2021-10-15 01:52:43 -0600

    keep non-py files on install

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/rcarroll901/django-orm-autodoc",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3.9",
         "Operating System :: MacOS",
